3a15bb9ff7f5ce628a30fb5132d59469aec7d917
karenCalulo199x <karenCalulo199x@github.com>
2024-11-11 21:02:23 +0800

    Added Password Viewmodel and password verification logic

--- app/src/main/java/com/kcals/loyaltyapp/view/fragment/PasswordFragment.kt
+++ app/src/main/java/com/kcals/loyaltyapp/view/fragment/PasswordFragment.kt
@@ -1,12 +1,25 @@
 package com.kcals.loyaltyapp.view.fragment
 
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.viewModelScope
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.kcals.loyaltyapp.base.BaseFragment
 import com.kcals.loyaltyapp.databinding.FragmentPasswordBinding
+import com.kcals.loyaltyapp.viewmodel.PasswordInput
+import com.kcals.loyaltyapp.viewmodel.PasswordState
+import com.kcals.loyaltyapp.viewmodel.PasswordViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class PasswordFragment : BaseFragment<FragmentPasswordBinding>() {
+    private val viewModel: PasswordViewModel by viewModels()
+
     override fun getViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
@@ -15,9 +28,64 @@ class PasswordFragment : BaseFragment<FragmentPasswordBinding>() {
     }
 
     override fun initViews() {
+        initViewModels()
         binding.apply {
 
+            etCurrentPassword.removeEmptyError(PasswordInput.CURRENT)
+            etNewPassword.removeEmptyError(PasswordInput.NEW)
+            etRetypePassword.removeEmptyError(PasswordInput.RETYPE)
+
+            saveBtn.setOnClickListener {
+                val current = etCurrentPassword.text.toString()
+                val new = etNewPassword.text.toString()
+                val retype = etRetypePassword.text.toString()
+                viewModel.apply {
+                    verifyCurrentPass(current)
+                    verifyNewPass(new)
+                    verifyRetypePass(new, retype)
+                }
+            }
         }
+    }
+
+    private fun initViewModels() {
+        viewModel.apply {
+            viewModelScope.launch {
+                currentPassState.collect { state ->
+                    statePasswordBinding(state, binding.etCurrentPasswordLayout)
+                }
+            }
+            viewModelScope.launch {
+                newPassState.collect { state ->
+                    statePasswordBinding(state, binding.etNewPasswordLayout)
+                }
+            }
+            viewModelScope.launch {
+                reTypePassState.collect { state ->
+                    statePasswordBinding(state, binding.etRetypePasswordLayout)
+                }
+            }
+        }
+    }
+
+    private fun statePasswordBinding(state: PasswordState, view: TextInputLayout) {
+        when (state) {
+            is PasswordState.Success -> view.error = null
+            is PasswordState.Idle -> view.error = null
+            is PasswordState.Error -> view.error = state.message
+        }
+    }
+
+    private fun TextInputEditText.removeEmptyError(passInput: PasswordInput) {
+        this.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.isNullOrEmpty()) viewModel.fieldFilledUp(passInput)
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 
 
---
+++ app/src/main/java/com/kcals/loyaltyapp/viewmodel/PasswordViewModel.kt
@@ -0,0 +1,71 @@
+package com.kcals.loyaltyapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class PasswordViewModel @Inject constructor() : ViewModel() {
+    private val _currentPassState = MutableStateFlow<PasswordState>(PasswordState.Idle)
+    val currentPassState: StateFlow<PasswordState> get() = _currentPassState
+
+    private val _newPassState = MutableStateFlow<PasswordState>(PasswordState.Idle)
+    val newPassState: StateFlow<PasswordState> get() = _newPassState
+
+    private val _reTypePassState = MutableStateFlow<PasswordState>(PasswordState.Idle)
+    val reTypePassState: StateFlow<PasswordState> get() = _reTypePassState
+
+    fun verifyCurrentPass(pass: String) {
+        viewModelScope.launch {
+            _currentPassState.value = when {
+                pass.isEmpty() -> PasswordState.Error("Empty")
+                pass == "mock123" -> PasswordState.Success
+                else -> PasswordState.Error("Incorrect")
+            }
+        }
+    }
+
+    fun verifyNewPass(newPass: String) {
+        viewModelScope.launch {
+            _newPassState.value = when {
+                newPass.isEmpty() -> PasswordState.Error("Empty")
+                newPass.length >= 8 -> PasswordState.Success
+                else -> PasswordState.Error("Password should be longer than 8 characters")
+            }
+        }
+    }
+
+    fun verifyRetypePass(newPass: String, retype: String) {
+        viewModelScope.launch {
+            _reTypePassState.value = when {
+                retype.isEmpty() -> PasswordState.Error("Empty")
+                retype == newPass -> PasswordState.Success
+                else -> PasswordState.Error("Mismatch")
+            }
+        }
+    }
+
+    fun fieldFilledUp(passInput: PasswordInput) {
+        when (passInput) {
+            PasswordInput.CURRENT -> _currentPassState.value = PasswordState.Idle
+            PasswordInput.NEW -> _newPassState.value = PasswordState.Idle
+            PasswordInput.RETYPE -> _reTypePassState.value = PasswordState.Idle
+
+        }
+    }
+
+}
+
+sealed class PasswordState {
+    data object Success : PasswordState()
+    data object Idle : PasswordState()
+    data class Error(val message: String) : PasswordState()
+}
+
+enum class PasswordInput {
+    CURRENT, NEW, RETYPE
+}
